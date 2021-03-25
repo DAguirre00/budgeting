1fccc30957333846893c3df8a04e6011fe2dc217
Dominic Aguirre <dominicaguirre890@gmail.com>
2021-03-25 11:31:32 -0400

    Started implemeting the checking and savings progam. More work is needed to implement it too the windows form and get date from the windows form.

---
+++ CheckingSavings.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Big_Project_practice
+{
+    class CheckingSavings
+    {
+        double savings, checking, amount, transferAmount;
+        char account;
+        bool transferToo;
+
+        public void withdraw(double savings, double checking, double amount)
+        {
+			if (//account == 's' need to make it so it looks at the form)
+			{
+				if (amount <= savings)
+				{
+					savings =- amount;
+				}
+				else
+				{
+					//cout << "You dont have enough money." << endl;
+				}
+			}
+			else if (//account == 'c' need to make it so it looks at the form)
+			{
+				if (amount <= checking)
+				{
+					checking =- amount;
+				}
+				else if (amount <= (checking + savings))
+				{ // Overdraft protection
+					//cout << "Not enough money in checking trasfering from savings." << endl;
+					amount =- checking;
+					checking = 0;
+					savings =- amount;
+				}
+				else
+				{
+					//cout << "You dont have enough money." << endl;
+				}
+			}
+			else
+			{
+				//cout << "Enter a proper account." << endl;
+			}
+		}
+        public void deposit(double savings, double checking, double amount)
+        {
+			if (//account == 's')
+			{
+				savings = + amount;
+			}
+			else if (account == 'c')
+			{
+				checking = + amount;
+			}
+			else
+			{
+				//cout << "Enter a proper account." << endl;
+			}
+			
+		}
+        public void trasnfer(double savings, double checking,double transferAmount, bool trasnferToo)
+        {
+			if (trasnferToo == true)
+			{
+				if (transferAmount <= savings)
+				{
+					checking = checking + transferAmount;
+					savings = savings - transferAmount;
+				}
+				else
+				{
+					//cout << "You dont have enough money" << endl;
+				}
+			}
+			else if (trasnferToo == false)
+			{
+				if (transferAmount <= checking)
+				{
+					checking = checking - transferAmount;
+					savings = savings + transferAmount;
+				}
+				else
+				{
+					//cout << "You dont have enough money." << endl;
+				}
+			}
+			else
+			{
+				// << "Enter a valid option." << endl;
+			}
+		}
+        public void check()
+        {
+
+        }
+
+    }
+}
