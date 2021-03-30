0bd32ed1545c6060eeb14125cc2d122ef756c79c
DAguirre00 <dominicaguirre890@gmail.com>
2021-03-30 13:06:07 -0400

    Checking and savings works just need to add logic to prevent people bugs

--- CheckingSavings.cs
+++ CheckingSavings.cs
@@ -6,7 +6,7 @@ namespace Big_Project_practice
 {
     class CheckingSavings
     {
-        double savings, checking, transferAmount,test2;
+        double savings, checking, transferAmount;
 		
         bool transferToo;
 
@@ -24,91 +24,116 @@ namespace Big_Project_practice
 			set { account = value; }
 		}
 
-		public void withdraw(double savings, double checking, double amount)
-        {
-			if (account == 's')
-			{
-				if (amount <= savings)
-				{
-					savings =- amount;
-				}
-				else
-				{
-					//cout << "You dont have enough money." << endl;
-				}
-			}
-			else if (account == 'c')
-			{
-				if (amount <= checking)
-				{
-					checking =- amount;
-				}
-				else if (amount <= (checking + savings))
-				{ // Overdraft protection
-					//cout << "Not enough money in checking trasfering from savings." << endl;
-					amount =- checking;
-					checking = 0;
-					savings =- amount;
-				}
-				else
-				{
-					//cout << "You dont have enough money." << endl;
-				}
-			}
-			else
-			{
-				//cout << "Enter a proper account." << endl;
-			}
+		private string cTotal;
+		public string CTotal
+		{
+			get { return cTotal; }
+			set { cTotal = value; }
 		}
-        public void Deposit()
+
+		private string sTotal;
+		public string STotal
+		{
+			get { return sTotal; }
+			set { sTotal = value; }
+		}
+
+		//public void withdraw(double savings, double checking, double amount)
+		//      {
+		//	if (account == 's')
+		//	{
+		//		if (amount <= savings)
+		//		{
+		//			savings =- amount;
+		//		}
+		//		else
+		//		{
+		//			//cout << "You dont have enough money." << endl;
+		//		}
+		//	}
+		//	else if (account == 'c')
+		//	{
+		//		if (amount <= checking)
+		//		{
+		//			checking =- amount;
+		//		}
+		//		else if (amount <= (checking + savings))
+		//		{ // Overdraft protection
+		//			//cout << "Not enough money in checking trasfering from savings." << endl;
+		//			amount =- checking;
+		//			checking = 0;
+		//			savings =- amount;
+		//		}
+		//		else
+		//		{
+		//			//cout << "You dont have enough money." << endl;
+		//		}
+		//	}
+		//	else
+		//	{
+		//		//cout << "Enter a proper account." << endl;
+		//	}
+		//}
+		public string Deposit()
         {
-			int temp = Convert.ToInt32(Amount);
+			double tempAmount = Convert.ToDouble(Amount);
+			
+			
 
 			if (Account.Contains("Savings"))
 			{
-				savings =+ temp;
+				double tempSTotal = Convert.ToDouble(STotal);
+				tempSTotal += tempAmount;
+				return Convert.ToString(tempSTotal);
+				
 			}
 			else if (Account.Contains("Checking"))
 			{
-				checking =+ temp;
+				double tempCTotal = Convert.ToDouble(CTotal);
+				tempCTotal += tempAmount;
+				return Convert.ToString(tempCTotal);
 			}
+            else
+            {
+				return "Error";
+            }
 			
 		}
-        public void trasnfer(double savings, double checking,double transferAmount, bool trasnferToo)
-        {
-			if (trasnferToo == true)
-			{
-				if (transferAmount <= savings)
-				{
-					checking = checking + transferAmount;
-					savings = savings - transferAmount;
-				}
-				else
-				{
-					//cout << "You dont have enough money" << endl;
-				}
-			}
-			else if (trasnferToo == false)
-			{
-				if (transferAmount <= checking)
-				{
-					checking = checking - transferAmount;
-					savings = savings + transferAmount;
-				}
-				else
-				{
-					//cout << "You dont have enough money." << endl;
-				}
-			}
-			else
-			{
-				// << "Enter a valid option." << endl;
-			}
-		}
-        public void Check()
-        {
+  //      public void trasnfer(double savings, double checking,double transferAmount, bool trasnferToo)
+  //      {
+		//	if (trasnferToo == true)
+		//	{
+		//		if (transferAmount <= savings)
+		//		{
+		//			checking = checking + transferAmount;
+		//			savings = savings - transferAmount;
+		//		}
+		//		else
+		//		{
+		//			//cout << "You dont have enough money" << endl;
+		//		}
+		//	}
+		//	else if (trasnferToo == false)
+		//	{
+		//		if (transferAmount <= checking)
+		//		{
+		//			checking = checking - transferAmount;
+		//			savings = savings + transferAmount;
+		//		}
+		//		else
+		//		{
+		//			//cout << "You dont have enough money." << endl;
+		//		}
+		//	}
+		//	else
+		//	{
+		//		// << "Enter a valid option." << endl;
+		//	}
+		//}
+  //      public void Check()
+  //      {
 
-        }
+  //      }
 
     }
 }
