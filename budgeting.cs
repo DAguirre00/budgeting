25c41e5428ebf3edf94184af165e66c6df0366ed
Sly467 <cooper.beaudet@snhu.edu>
2021-04-01 13:41:50 -0400

    added budgeting class and calculated_savings  and time_to_save methodsReduced State amount

---
+++ budgeting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Big_Project_practice
+{
+    class Budgeting
+
+    {
+
+        public double Calculated_savings(double income, double tax, double rent, double food, double transport)
+        {
+
+            double calc_savings = income - tax - rent - food - transport;
+
+            return calc_savings;
+        }
+
+        public double Time_To_Save(double calc_savings, double init_savings, double cost)
+        {
+            double time = 0;
+
+            cost = cost - init_savings;
+            time= cost/calc_savings;
+
+            return time;
+        }
+
+    }
+}
